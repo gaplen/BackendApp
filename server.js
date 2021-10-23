9ec4272bbc8d35e8571ae6fa4d6d6a533338fb2a
gaplen <lgp221301@gmail.com>
2021-10-22 21:01:48 -0500

    Update server.js

--- server.js
+++ server.js
@@ -75,7 +75,7 @@ orders(app);
 products(app, upload);
 mercadoPagoRoutes(app);
 
-server.listen(8080, '157.245.91.4' || 'localhost', function() {
+server.listen(3000, '157.245.91.4' || 'localhost', function() {
     console.log('Aplicacion de NodeJS ' + port + ' Iniciada...')
 });
 
